a1574934ec6c456c68e27497d41666d9647e6c42
IamRoman-rb <roman.borla2004@gmail.com>
2026-01-05 15:16:33 -0300

    Generando archivo Auth.jsx

---
+++ client/src/Auth.jsx
@@ -0,0 +1,12 @@
+const path = {};
+
+path.admin = [
+];
+path.propietario = [
+];
+path.supervisor = [
+];
+path.empleado = [
+];
+
+export default path;
--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,27 +1,41 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Default from "./Layouts/Default";
 import Login from "./Pages/Website/Login";
 import Logout from "./Pages/Website/Logout";
 import Perfil from "./Pages/Website/Perfil";
+import Autenticados from "./Layouts/Autenticados";
+import ListadoClientes from "./Pages/Website/Clientes/ListadoClientes";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Default />,
     children: [
       {
+        index: true,
+        element: <Navigate to={"login"} />,
+      },
+      {
         path: "login",
         element: <Login />,
       },
       {
-        path: "logout",
-        element: <Logout />,
-      },
-      {
-        path: "perfil",
-        element: <Perfil />,
-      },
-      {
-        
+        path: "admin",
+        element: <Autenticados />,
+        children: [
+          {
+            index: true,
+            element: <ListadoClientes />,
+          },
+          {
+            path: "logout",
+            element: <Logout />,
+          },
+          {
+            path: "perfil",
+            element: <Perfil />,
+          },
+        ],
+
       }
     ]
   }
